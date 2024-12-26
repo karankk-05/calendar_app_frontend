19128c25a4bfa761e2829c864c53c39349d68773
karankk.05 <karank23@iitk.ac.in>
2024-12-26 19:39:14 +0530

    Integrated API

---
+++ Calendar_Application/android/app/src/main/kotlin/com/example/optiparse/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.Calendar_Application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Calendar_Application/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:calendar_application/features/auth/screens/login_screen.dart';
+import 'package:calendar_application/features/home/controllers/calendar_grid_controller.dart';
+import 'package:calendar_application/features/home/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:calendar_application/themes/dark_theme.dart';
+import 'package:calendar_application/themes/light_theme.dart';
+import 'package:provider/provider.dart';  
+
+void main() async {
+  //fetchSlots();
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure that bindings are initialized before running the app
+  String? username = await getUsernameFromStorage(); // Retrieve the username
+  runApp(MyApp(username: username));
+}
+
+Future<String?> getUsernameFromStorage() async {
+  final storage = FlutterSecureStorage();
+  return await storage.read(key: 'username'); // Retrieve the username saved under 'username' key
+}
+
+class MyApp extends StatelessWidget {
+  final String? username;
+  static String baseUrl = 'https://winter-intern-task.onrender.com';
+  const MyApp({super.key, required this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return  ChangeNotifierProvider(
+      create: (_) => CalendarController(),
+       child:MaterialApp(
+      title: 'Calendar Application',
+      theme: LightTheme.theme,
+      darkTheme: DarkTheme.theme,
+      themeMode: ThemeMode.system,
+      home: username == null || (!username!.contains('Karan') && !username!.contains('Lorem'))
+          ? const LoginScreen()
+          : const HomeScreen(), // Conditional redirection based on username
+      
+     ) );
+  }
+}
